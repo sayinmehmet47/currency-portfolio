08e46314f1582ad566929efe99c21499e6134597
unknown <sayinmehmet47@gmail.com>
2021-11-07 14:01:28 +0200

    added 12 responsive cards

--- src/components/DailyNews.js
+++ src/components/DailyNews.js
@@ -1,5 +1,6 @@
 import Button from "@restart/ui/esm/Button";
 import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import {
   Card,
@@ -15,94 +16,33 @@ import { currencyNews } from "../store/Actions/currencyActions";
 
 export default function DailyNews() {
   const dispatch = useDispatch();
-
+  const news = useSelector((state) => state.currencyNews.news);
   useEffect(() => {
-    dispatch(currencyNews(10));
+    dispatch(currencyNews(12));
   }, []);
 
   return (
     <div className="container">
       <h2 class="TrendingNow-heading">Trending Now</h2>
-      <Row lg={9} md={3} className="d-flex justify-content-center">
-        <Card className="m-2">
-          <CardImg
-            alt="Card image cap"
-            src="https://picsum.photos/318/180"
-            top
-            width="100%"
-          />
-          <CardBody>
-            <CardTitle tag="h5">Card title</CardTitle>
-            <CardSubtitle className="mb-2 text-muted" tag="h6">
-              Card subtitle
-            </CardSubtitle>
-            <CardText>
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </CardText>
-            <Button>Button</Button>
-          </CardBody>
-        </Card>
-        <Card className="m-2">
-          <CardImg
-            alt="Card image cap"
-            src="https://picsum.photos/318/180"
-            top
-            width="100%"
-          />
-          <CardBody>
-            <CardTitle tag="h5">Card title</CardTitle>
-            <CardSubtitle className="mb-2 text-muted" tag="h6">
-              Card subtitle
-            </CardSubtitle>
-            <CardText>
-              This card has supporting text below as a natural lead-in to
-              additional content.
-            </CardText>
-            <Button>Button</Button>
-          </CardBody>
-        </Card>
-        <Card className="m-2">
-          <CardImg
-            alt="Card image cap"
-            src="https://picsum.photos/318/180"
-            top
-            width="100%"
-          />
-          <CardBody>
-            <CardTitle tag="h5">Card title</CardTitle>
-            <CardSubtitle className="mb-2 text-muted" tag="h6">
-              Card subtitle
-            </CardSubtitle>
-            <CardText>
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This card has even longer content
-              than the first to show that equal height action.
-            </CardText>
-            <Button>Button</Button>
-          </CardBody>
-        </Card>
-        <Card className="m-2">
-          <CardImg
-            alt="Card image cap"
-            src="https://picsum.photos/318/180"
-            top
-            width="100%"
-          />
-          <CardBody>
-            <CardTitle tag="h5">Card title</CardTitle>
-            <CardSubtitle className="mb-2 text-muted" tag="h6">
-              Card subtitle
-            </CardSubtitle>
-            <CardText>
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This card has even longer content
-              than the first to show that equal height action.
-            </CardText>
-            <Button>Button</Button>
-          </CardBody>
-        </Card>
+      <Row lg={4} md={3} className="d-flex justify-content-center">
+        {news.map((e) => {
+          return (
+            <Card className="m-2 shadow rounded">
+              <CardImg
+                alt="Card image cap"
+                src={e.promoImage.url}
+                top
+                width="100%"
+              />
+              <CardBody>
+                <CardSubtitle className="mb-2 text-muted" tag="h6">
+                  {e.headline}
+                </CardSubtitle>
+                <CardText>{e.description}</CardText>
+              </CardBody>
+            </Card>
+          );
+        })}
       </Row>
     </div>
   );
