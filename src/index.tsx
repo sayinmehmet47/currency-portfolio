22271942a417dc5300c0dc72b4cef6634c2b8302
mehmet <sayinmehmet47@gmail.com>
2022-09-20 17:23:13 +0200

    feat:typescript add

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import './i18n/config';
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
 import { store } from './store/store';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import persistStore from 'redux-persist/es/persistStore';
@@ -22,8 +21,3 @@ ReactDOM.render(
 
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
