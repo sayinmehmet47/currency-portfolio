38eceb74a45bc5708c1591bc9ee57a154d2cc206
mehmet <sayinmehmet47@gmail.com>
2023-01-27 21:43:28 +0100

    feat: first test with playwrite

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,10 +4,11 @@ export default defineConfig({
   webServer: {
     command: 'npm run start',
     port: 3000,
-    timeout: 520000,
+    timeout: 120000,
     reuseExistingServer: !process.env.CI,
   },
   use: {
     baseURL: 'http://localhost:3000/',
+    trace: 'on-first-retry', // record traces on first retry of each test
   },
 });
